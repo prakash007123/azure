ab3c2f9726258b3d0edb6c7297d402644112ee5a
prakashgnanaraj <prakashgnanaraj1999@gmail.com>
2023-07-12 17:45:36 +0530

    5

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Index Page</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Index Page</h1>
 
-require __DIR__ . '/vendor/autoload.php';
-use Carbon\Carbon;
+    <?php
+        // PHP code can be inserted here
 
+        // Example: Displaying the current date and time
+        echo "Current date and time: " . date("Y-m-d H:i:s");
+    ?>
 
-printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
-
-printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
-
-printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+    <p>This is a simple PHP index page.</p>
+</body>
+</html>
